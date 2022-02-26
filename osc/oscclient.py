83c235613cd51e459ee27be486d87fd07f60f1ae
kernmartin <martin.kern@outlook.com>
2022-02-26 21:40:04 +0100

    erase

--- osc/oscclient.py
+++
@@ -1,9 +0,0 @@
-from pythonosc.udp_client import SimpleUDPClient
-
-ip = "192.168.0.17"
-port = 5432
-
-client = SimpleUDPClient(ip, port)  # Create client
-
-client.send_message("/some/address", 123)   # Send float message
-client.send_message("/some/address", [1, 2., "hello"])  # Send message with int, float and string
