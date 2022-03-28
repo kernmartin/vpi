4c87d14143b649aeeba0ca08a3bd7d57803b6e8d
Georg Kettele <kg@kmkg.org>
2022-03-28 13:15:42 +0200

    Create vpimotorfunctions.py

---
+++ vpimotorfunctions.py
@@ -0,0 +1,25 @@
+def calculateStepsDestination(destination, direction, over=0) 
+    
+    steps = 0
+
+    if direction == 1:
+        #CW
+        if destination > POS:
+            steps = destination - POS
+        elif destination < POS
+            steps = 360 - destination
+        elif destination == POS
+            steps = 0
+            
+    elif direction = 0:
+        # CCW
+        if destination > POS:
+            steps = 360 - destination - POS
+        elif destination < POS
+            steps = POS - destinatin
+        elif destination == POS
+            steps = 0
+    
+    # Drüberdrehen wie oft         
+    if over != 0:
+        steps = steps + over * 360
