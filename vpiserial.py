46de144023845cefd5ef7e4aaebe9acfc451af26
kernmartin <martin.kern@outlook.com>
2022-03-29 20:26:29 +0200

    Update vpiserial.py

--- vpiserial.py
+++ vpiserial.py
@@ -8,10 +8,11 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
     einmal = 0
     while True:
-        if(einmal == 0):
-            ser.write(str("1001180\n").encode('utf-8'))
-            einmal = einmal + 1
+        ser.write(str("1001001\n").encode('utf-8'))
+        einmal = einmal + 1
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
-        time.sleep(5)
-        sys.exit(0)
+        time.sleep(1)
+        einmal = einmal + 1
+        if(einmal == 100):
+            sys.exit()
