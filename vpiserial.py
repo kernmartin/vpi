7324bfe01bd68947a3d53fc371424d0bc00abc52
kernmartin <martin.kern@outlook.com>
2022-03-29 20:28:22 +0200

    Update vpiserial.py

--- vpiserial.py
+++ vpiserial.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
     einmal = 0
+    ser.write(str("1001001\n").encode('utf-8'))
     while True:
-        ser.write(str("1001001\n").encode('utf-8'))
+
         einmal = einmal + 1
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
