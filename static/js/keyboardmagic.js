0b663675e3e6c776eb3f234bd9c0dca5b3e2b9bc
kernmartin <martin.kern@outlook.com>
2022-03-30 20:53:50 +0200

    Update keyboardmagic.js

--- static/js/keyboardmagic.js
+++ static/js/keyboardmagic.js
@@ -21,7 +21,7 @@ $(function() {
     usePreview: true,
     customLayout: {
       'normal': [
-        '1 2 3 4 5 6 7 8 9 0',
+        '1 2 3',
         '4 5 6',
         '7 8 9',
         '0 {left} {right}',
