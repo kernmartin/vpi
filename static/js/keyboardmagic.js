830907b4df91520c198c4896457bc09bfdc8e397
kernmartin <martin.kern@outlook.com>
2022-03-30 20:41:37 +0200

    d

--- static/js/keyboardmagic.js
+++ static/js/keyboardmagic.js
@@ -6,10 +6,10 @@ $(function() {
     layout: 'custom',
     customLayout: {
       'normal': [
-        '1 2 3 4 5 6 7 8 9 0 - {bksp}',
+        '1 2 3 4 5 6 7 8 9 0',
         'q w e r t y u i o p',
-        'a s d f g h j k l',
-        'z x c v b n m',
+        'a s d f g h j k l -',
+        'z x c v b n m {bksp}',
         '{accept} {space} {left} {right}'
       ]
     }
